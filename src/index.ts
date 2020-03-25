c0c4f32c8c5921df1fb681326d00680ad168b9ab
reji <60493584+rejik2@users.noreply.github.com>
2020-03-25 15:39:24 -0700

    Reji/update (#2)
    * update npm packages
    
    * updating test.ts
    
    * adding promise and async support
    
    * adding awaits

--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,83 @@
 import '@k2oss/k2-broker-core';
 
 metadata = {
-	"systemName": "MSGraphSharePoint",
-	"displayName": "Microsoft Graph - SharePoint Broker",
-	"description": "Sample broker for SharePoint using MSGraph"
+    "systemName": "MSGraphSharePoint",
+    "displayName": "Microsoft Graph - SharePoint Broker",
+    "description": "Sample broker for SharePoint using MSGraph"
 };
 
-ondescribe = function() {
-    postSchema({ objects: {
-                "list": {
-                    displayName: "List",
-                    description: "SharePoint List",
-                    properties: {
-                        "id": {
-                            displayName: "ID",
-                            type: "string" 
-                        },
-                        "name": {
-                            displayName: "Name",
-                            type: "string" 
-                        }
+ondescribe = async function (): Promise<void> {
+    postSchema({
+        objects: {
+            "list": {
+                displayName: "List",
+                description: "SharePoint List",
+                properties: {
+                    "id": {
+                        displayName: "ID",
+                        type: "string"
                     },
-                    methods: {
-                        "get": {
-                            displayName: "Get List",
-                            type: "list",
-                            outputs: [ "id", "name" ]
-                        }
+                    "name": {
+                        displayName: "Name",
+                        type: "string"
+                    }
+                },
+                methods: {
+                    "get": {
+                        displayName: "Get List",
+                        type: "list",
+                        outputs: ["id", "name"]
                     }
                 }
             }
         }
-    )};
+    }
+    )
+};
 
-onexecute = function(objectName, methodName, parameters, properties) {
-    switch (objectName)
-    {
-        case "list": onexecuteList(methodName, parameters, properties); break;
+onexecute = async function (objectName, methodName, parameters, properties): Promise<void> {
+    switch (objectName) {
+        case "list": await onexecuteList(methodName, parameters, properties); break;
         default: throw new Error("The object " + objectName + " is not supported.");
     }
 }
 
-function onexecuteList(methodName: string, parameters: SingleRecord, properties: SingleRecord) {
-    switch (methodName)
-    {
-        case "get": onexecuteListGet(parameters, properties); break;
+async function onexecuteList(methodName: string, parameters: SingleRecord, properties: SingleRecord): Promise<void> {
+    switch (methodName) {
+        case "get": await onexecuteListGet(parameters, properties); break;
         default: throw new Error("The method " + methodName + " is not supported.");
     }
 }
 
-function onexecuteListGet(parameters: SingleRecord, properties: SingleRecord) {
-    var xhr = new XMLHttpRequest();
+function onexecuteListGet(parameters: SingleRecord, properties: SingleRecord): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        var xhr = new XMLHttpRequest();
 
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState !== 4) return;
-        if (xhr.status !== 200) throw new Error("Failed with status " + xhr.status);
+        xhr.onreadystatechange = function () {
+            try {
+                if (xhr.readyState !== 4) return;
+                if (xhr.status !== 200) throw new Error("Failed with status " + xhr.status);
 
-        var obj = JSON.parse(xhr.responseText);
-        postResult(obj.map(x => {
-            return {
-                "id": x.id,
-                "name": x.name
+                var obj = JSON.parse(xhr.responseText);
+                postResult(obj.map(x => {
+                    return {
+                        "id": x.id,
+                        "name": x.name
+                    }
+                }));
+                resolve();
+            } catch (error) {
+                reject();
             }
-        }));
-    };
+        };
 
-    var url = "https://graph.microsoft.com/v1.0/sites/root/lists";
+        var url = "https://graph.microsoft.com/v1.0/sites/root/lists";
 
-    xhr.open("GET", url);
-    // Authentication Header
-    // Use .withCredentials to use service instance configured OAuth (Bearer) or Static (Basic)
-    // Anything else, don't set .withCredentials and use .setRequestHeader to set the Authentication header
-    xhr.withCredentials = true;
-    xhr.send();
+        xhr.open("GET", url);
+        // Authentication Header
+        // Use .withCredentials to use service instance configured OAuth (Bearer) or Static (Basic)
+        // Anything else, don't set .withCredentials and use .setRequestHeader to set the Authentication header
+        xhr.withCredentials = true;
+        xhr.send();
+    });
 }
